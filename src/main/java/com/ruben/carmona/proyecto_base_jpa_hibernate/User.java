bdd900ca1440136cce673df53fef3b43ba8f3e79
Rubén Carmona <carmonagarcia@gmail.com>
2021-12-04 19:20:52 +0100

    Iniciamos repositorio.

---
+++ src/main/java/com/ruben/carmona/proyecto_base_jpa_hibernate/User.java
@@ -0,0 +1,51 @@
+package com.ruben.carmona.proyecto_base_jpa_hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	private int id;
+	
+	@Column
+	private String userName;
+	
+	@Column
+	private String userMesagge;	
+	
+
+	public User() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserMesagge() {
+		return userMesagge;
+	}
+
+	public void setUserMesagge(String userMesagge) {
+		this.userMesagge = userMesagge;
+	}
+	
+	
+	
+	
+}
